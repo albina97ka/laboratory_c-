c2c6b909f9833158d088d61fac4e761a45082f52
albina97ka <122766344+albina97ka@users.noreply.github.com>
2024-05-09 23:21:38 +0300

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+delegate void EventHandler(string message);
+
+class FirstClass
+{
+    public event EventHandler EventOccured;
+    private string name;
+
+    public FirstClass(string name)
+    {
+        this.name = name;
+    }
+
+    public void RaiseEvent()
+    {
+        EventOccured?.Invoke(name);
+    }
+}
+
+class SecondClass
+{
+    private string name;
+
+    public SecondClass(string name)
+    {
+        this.name = name;
+    }
+
+    public void HandleEvent(string eventName)
+    {
+        Console.WriteLine($"Событие {eventName} вызвано через объект {name}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FirstClass firstObject1 = new FirstClass("Первый объект 1");
+        FirstClass firstObject2 = new FirstClass("Первый объект 2");
+        SecondClass secondObject = new SecondClass("Второй объект");
+
+        firstObject1.EventOccured += secondObject.HandleEvent;
+        firstObject2.EventOccured += secondObject.HandleEvent;
+
+        firstObject1.RaiseEvent();
+        firstObject2.RaiseEvent();
+    }
+}
---
+++ Program11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    delegate string GetNextDayDelegate();
+    static void Main(string[] args)
+    {
+        string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int currentIndex = 0;
+
+        GetNextDayDelegate getNextDay = () =>
+        {
+            int nextIndex = (currentIndex) % 7;
+            currentIndex += 1;
+            return daysOfWeek[nextIndex];
+        };
+
+        for (int i = 0; i < 14; i++)
+        {
+            Console.WriteLine(getNextDay());
+        }
+    }
+}
